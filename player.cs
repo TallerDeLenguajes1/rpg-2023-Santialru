a99cc4ca766e0e7add7a3ce5c9bd491370034328
Santiago <alvarezrubiosanti@gmail.com>
2023-06-15 17:38:18 -0300

    primer commit

---
+++ player.cs
@@ -0,0 +1,17 @@
+namespace personaje;
+using System;
+
+public class perso
+{
+    private string tipo;
+    private string Nombre;
+    private string Apodo;
+    private DateTime fecha;
+    private int edad;
+    public int velocidad;
+    public int destreza;
+    public int fuerza;
+    public int nivel;
+    public int armadura;
+    public int salud;
+}
